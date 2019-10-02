22695fa744d0cdd18a5f7610f4b2cc93deb4bf2e
mario <mario@synapbox.com>
2019-10-02 17:07:21 -0600

    rename variables of dictionaries and lists

--- syn-gcp-objects/objects.py
+++ syn-gcp-objects/objects.py
@@ -28,60 +28,63 @@ class objects:
 		
 	def get_objects(self,result):
 		self.logger.info('OBJECTS')
-		list_objs = []
+		object_list = []
 		for i, obj in enumerate(result.annotation_results[0].object_annotations):
 			if(len(obj.entity.description)>0):
 				prev_ms = str(round((obj.segment.start_time_offset.seconds + obj.segment.start_time_offset.nanos/1e9)*1000,4))
-				list_sub = []
-				objects_sub_dict = {}
+				appearance_list = []
+				appearance_dict = {}
 				for obj_f in obj.frames:
 					box = obj_f.normalized_bounding_box
 					
 					ms_current = str(round((obj_f.time_offset.seconds+obj_f.time_offset.nanos/1e9)*1000,4))
 					
-					objects_sub_dict["left"] = round(box.left,4)
-					objects_sub_dict["top"] = round(box.top,4)
-					objects_sub_dict["right"] = round(box.right,4)
-					objects_sub_dict["bottom"] = round(box.bottom,4)
-					objects_sub_dict["start"] = prev_ms
-					objects_sub_dict["end"] = ms_current
-					objects_sub_dict["confidence"] = round(obj.confidence,4)
+					appearance_dict["left"] = round(box.left,4)
+					appearance_dict["top"] = round(box.top,4)
+					appearance_dict["right"] = round(box.right,4)
+					appearance_dict["bottom"] = round(box.bottom,4)
+					appearance_dict["start"] = prev_ms
+					appearance_dict["end"] = ms_current
+					appearance_dict["confidence"] = round(obj.confidence,4)
 					
-					list_sub.append(objects_sub_dict)
-					objects_sub_dict = {}
+					appearance_list.append(appearance_dict)
+					appearance_dict = {}
 
 					prev_ms = ms_current
-				exist = self.check_existence(list_objs,obj.entity.description)
-				if exist:
-					list_objs = self.already_on_list(list_objs,obj.entity.description,list_sub)
-				else:
-					list_objs = self.new_on_list(list_objs,obj.entity.description,list_sub)
-		self.write_json(list_objs)
 
-	def check_existence(self,list_objs,name,list_sub):
+					object_name = obj.entity.description
+
+				exist = self.check_existence(object_list,object_name)
+				if exist:
+					object_list = self.already_on_list(object_list,object_name,appearance_list)
+				else:
+					object_list = self.new_on_list(object_list,object_name,appearance_list)
+		self.write_json(object_list)
+
+	def check_existence(self,object_list,name,appearance_list):
 		exist = False
-		for element in list_objs: 
+		for element in object_list: 
 			if(element['object']==name):
 				exist = True
 				break;	
 		return exist
 
-	def already_on_list(self,list_objs,name,list_sub):
-		for element in list_objs: 
+	def already_on_list(self,object_list,name,appearance_list):
+		for element in object_list: 
 			if(element['object']==name):
-				element['appearances'] += list_sub
+				element['appearances'] += appearance_list
 				break;
-		return list_objs
+		return object_list
 
-	def new_on_list(self,list_objs,name,list_sub):
-		objects_dict = {}
-		objects_dict["object"] = name
-		objects_dict["appearances"] = list_sub
-		list_objs.append(objects_dict)
-		return list_objs
+	def new_on_list(self,object_list,name,appearance_list):
+		object_dict = {}
+		object_dict["object"] = name
+		object_dict["appearances"] = appearance_list
+		object_list.append(object_dict)
+		return object_list
 
-	def write_json(self,list_objs):
+	def write_json(self,object_list):
 		bucket = 'syn-ai-aaoi'
 		s3 = boto3.resource("s3").Bucket(bucket)
 		json.dump_s3 = lambda obj, f: s3.Object(key='file.json').put(Body=json.dumps(obj))
-		json.dump_s3(list_objs,'file.json')
+		json.dump_s3(object_list,'file.json')
