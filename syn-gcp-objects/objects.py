2e74426cee406fb84f73c5d6c02e35943276c816
mario <mario@synapbox.com>
2019-10-02 18:53:00 -0600

    reorder methods

--- syn-gcp-objects/objects.py
+++ syn-gcp-objects/objects.py
@@ -35,15 +35,6 @@ class objects:
 				object_list = self.all_objects(obj.entity.description,appearance_list,object_list)
 		self.write_json(object_list)
 
-	def all_objects(self,object_name,appearance_list,object_list):
-		object_name = obj.entity.description
-		exist = self.check_existence(object_list,object_name)
-		if exist:
-			object_list = self.already_on_list(object_list,object_name,appearance_list)
-		else:
-			object_list = self.new_on_list(object_list,object_name,appearance_list)
-		return object_list
-
 	def all_appearances(self,initial_time,all_frames,confidence):
 		appearance_list = []
 		previous_milliseconds = self.milliseconds(obj.segment.start_time_offset)
@@ -53,6 +44,30 @@ class objects:
 			appearance_list.append(appearance_dict)
 			previous_milliseconds = current_millisecons
 		return appearance_list
+
+	def milliseconds(self,time):
+		milliseconds = str(round((time.seconds + time.nanos/1e9)*1000,4))
+    	return milliseconds
+
+    def one_appearance(self,box,previous_milliseconds,current_milliseconds,confidence):
+		appearance_dict = {}
+		appearance_dict["left"] = round(box.left,4)
+		appearance_dict["top"] = round(box.top,4)
+		appearance_dict["right"] = round(box.right,4)
+		appearance_dict["bottom"] = round(box.bottom,4)
+		appearance_dict["start"] = previous_milliseconds
+		appearance_dict["end"] = current_milliseconds
+		appearance_dict["confidence"] = round(obj.confidence,4)
+		return appearance_dict		
+
+	def all_objects(self,object_name,appearance_list,object_list):
+		object_name = obj.entity.description
+		exist = self.check_existence(object_list,object_name)
+		if exist:
+			object_list = self.already_on_list(object_list,object_name,appearance_list)
+		else:
+			object_list = self.new_on_list(object_list,object_name,appearance_list)
+		return object_list
 
 	def check_existence(self,object_list,name,appearance_list):
 		exist = False
@@ -75,21 +90,6 @@ class objects:
 		object_dict["appearances"] = appearance_list
 		object_list.append(object_dict)
 		return object_list
-
-	def one_appearance(self,box,previous_milliseconds,current_milliseconds,confidence):
-		appearance_dict = {}
-		appearance_dict["left"] = round(box.left,4)
-		appearance_dict["top"] = round(box.top,4)
-		appearance_dict["right"] = round(box.right,4)
-		appearance_dict["bottom"] = round(box.bottom,4)
-		appearance_dict["start"] = previous_milliseconds
-		appearance_dict["end"] = current_milliseconds
-		appearance_dict["confidence"] = round(obj.confidence,4)
-		return appearance_dict		
-
-	def milliseconds(self,time):
-		milliseconds = str(round((time.seconds + time.nanos/1e9)*1000,4))
-    	return milliseconds
 
 	def write_json(self,object_list):
 		bucket = 'syn-ai-aaoi'
