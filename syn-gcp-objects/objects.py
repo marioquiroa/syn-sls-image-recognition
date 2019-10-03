2a7f9047b0e6d69effde7e17ec2edd328c2c60d0
mario <mario@synapbox.com>
2019-10-02 18:15:17 -0600

    method fill_apearance

--- syn-gcp-objects/objects.py
+++ syn-gcp-objects/objects.py
@@ -35,34 +35,20 @@ class objects:
 				previous_milliseconds = str(round((start_time.seconds + start_time.nanos/1e9)*1000,4))
 				
 				appearance_list = []
-				appearance_dict = {}
-				
 				for current_frame in obj.frames:
-					
 					end_time = current_frame.time_offset
-					current_millisecons = str(round((end_time.seconds+current_frame.time_offset.nanos/1e9)*1000,4))
-					
-					box = current_frame.normalized_bounding_box
-					appearance_dict["left"] = round(box.left,4)
-					appearance_dict["top"] = round(box.top,4)
-					appearance_dict["right"] = round(box.right,4)
-					appearance_dict["bottom"] = round(box.bottom,4)
-					appearance_dict["start"] = previous_milliseconds
-					appearance_dict["end"] = current_millisecons
-					appearance_dict["confidence"] = round(obj.confidence,4)
-					
+					current_millisecons = str(round((end_time.seconds+end_time.nanos/1e9)*1000,4))
+					self.fill_appearance(current_frame.normalized_bounding_box,previous_milliseconds,current_milliseconds,obj.confidence)
 					appearance_list.append(appearance_dict)
-					appearance_dict = {}
-
 					previous_milliseconds = current_millisecons
 
-					object_name = obj.entity.description
-
+				object_name = obj.entity.description
 				exist = self.check_existence(object_list,object_name)
 				if exist:
 					object_list = self.already_on_list(object_list,object_name,appearance_list)
 				else:
 					object_list = self.new_on_list(object_list,object_name,appearance_list)
+		
 		self.write_json(object_list)
 
 	def check_existence(self,object_list,name,appearance_list):
@@ -86,6 +72,17 @@ class objects:
 		object_dict["appearances"] = appearance_list
 		object_list.append(object_dict)
 		return object_list
+
+	def fill_appearance(self,box,previous_milliseconds,current_milliseconds,confidence):
+		appearance_dict = {}
+		appearance_dict["left"] = round(box.left,4)
+		appearance_dict["top"] = round(box.top,4)
+		appearance_dict["right"] = round(box.right,4)
+		appearance_dict["bottom"] = round(box.bottom,4)
+		appearance_dict["start"] = previous_milliseconds
+		appearance_dict["end"] = current_milliseconds
+		appearance_dict["confidence"] = round(obj.confidence,4)
+		return appearance_dict		
 
 	def write_json(self,object_list):
 		bucket = 'syn-ai-aaoi'
